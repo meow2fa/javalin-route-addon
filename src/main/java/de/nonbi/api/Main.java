4087e54ef3f6d2c25974c4c80baee79c335b964d
Nyx <nyx@veero.de>
2025-05-17 01:24:04 +0200

    cleanup Main.java

--- src/main/java/de/nonbi/api/Main.java
+++ src/main/java/de/nonbi/api/Main.java
@@ -4,8 +4,4 @@ import de.nonbi.api.route.AutoRouteManager;
 import io.javalin.Javalin;
 
 public class Main {
-    public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7002);
-        AutoRouteManager.registerRoutes(app, "de.nonbi.api.controller", false);
-    }
 }
